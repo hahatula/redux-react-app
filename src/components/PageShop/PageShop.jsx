0bbcb50b72477426837d79bbeb86148bf32f9337
hahatula <olgagolubev.pro@gmail.com>
2024-04-23 14:12:51 +0300

    search input element

--- src/components/PageShop/PageShop.jsx
+++ src/components/PageShop/PageShop.jsx
@@ -4,7 +4,7 @@ function PageShop() {
   return (
     <div className="shop">
       <h1 className="title">Welcome to the fake shop</h1>
-      <div className="search">search</div>
+      <div className="search"><input type="text" className="search__input"/></div>
       <div className="products">products</div>
       <div className="users">users</div>
     </div>
