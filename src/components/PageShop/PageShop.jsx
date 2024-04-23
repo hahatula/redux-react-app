cacd0f3bfbfbdade8eaf737df3e1bdcc6c9aa912
hahatula <olgagolubev.pro@gmail.com>
2024-04-23 15:02:40 +0300

    users and products markup

--- src/components/PageShop/PageShop.jsx
+++ src/components/PageShop/PageShop.jsx
@@ -4,9 +4,39 @@ function PageShop() {
   return (
     <div className="shop">
       <h1 className="title">Welcome to the fake shop</h1>
-      <div className="search"><input type="text" className="search__input"/></div>
-      <div className="products">products</div>
-      <div className="users">users</div>
+      <div className="search">
+        <input type="text" className="search__input" />
+      </div>
+      <ul className="products">
+        <li className="product">
+            <img src="#" alt="product photo" className="product__img"/>
+        </li>
+        <li className="product">
+            <img src="#" alt="product photo" className="product__img"/>
+        </li>
+        <li className="product">
+            <img src="#" alt="product photo" className="product__img"/>
+        </li>
+        <li className="product">
+            <img src="#" alt="product photo" className="product__img"/>
+        </li>
+        <li className="product">
+            <img src="#" alt="product photo" className="product__img"/>
+        </li>
+      </ul>
+      <ul className="users">
+        <h2 className="users__title">Users</h2>
+        <li className="user">
+            <p className="user__info">User name:</p>
+            <p className="user__info">Address:</p>
+            <p className="user__info">Phone number:</p>
+        </li>
+        <li className="user">
+            <p className="user__info">User name:</p>
+            <p className="user__info">Address:</p>
+            <p className="user__info">Phone number:</p>
+        </li>
+      </ul>
     </div>
   );
 }
